dbc4b5eb2242df2d616f086acbbe10894a1bd287
ArseniyUshakov <arseniyushakov113@gmail.com>
2024-12-17 21:24:16 +0500

    Add files via upload

---
+++ lab1/halfdiv/halfdiv/halfdiv.cpp
@@ -0,0 +1,44 @@
+﻿#include <cmath>
+#include <clocale>
+#include <iostream>
+using namespace std;
+double answer;
+double a;
+double b;
+double x0;
+bool rule = true;
+double xi = 0;
+double eps = pow(10, -6); //Точность решения
+double f(double x0) //Функция нелинейного уравнения
+{
+    return (x0 - (1 / (3 + sin(3.6 * x0))));
+}
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    cout << "Введите значение интервала [a; b] ";
+    cin >> a >> b;    
+    int n = 1;  
+    while (rule)
+    {
+        x0 = (a + b) / 2;
+        if (abs(x0 - xi) <= eps) //Проверка промежуточного значения корня
+        {
+            rule = false; //Завершаем цикл половинчатого деления
+        }
+        if (f(x0) * f(a) < 0) //Проверка на какой половине есть корень
+        {
+            b = x0;
+        }
+        else
+        {
+            a = x0; 
+        }
+        xi = x0;
+        cout << "Промежуточное значение " << n << " = " << xi << endl; //Вывод промежуточного значения
+        n++;
+    }
+    answer = xi;
+    cout << "Корень уравнения = " << answer;
+    return 0;
+}
---
+++ lab1/newton/newton/newton.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cmath>
+#include <clocale>
+using namespace std;
+double a = 0;
+double b = 0.85;
+double eps = pow(10, -6);
+double answer;
+double x;
+double xi;
+bool rule = true;
+double f(double x)
+{
+	return x - 1 / (3 + sin(3.6 * x));
+}
+double pf(double x)
+{
+	return 1 - (18 * cos((18 / 5) * x)) / (5 * pow((3 + sin((18 / 5) * x)), 2));
+}
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите любое значение х в интервале [0; 0.85]: ";
+	cin >> x;
+	if ((x >= a) && (x <= b)) { //Проверяем входит ли х в промежуток
+		int n = 1;
+		while (rule)
+		{
+			xi = x - f(x) / pf(x); //Касательная к графику
+			if (abs(x - xi) <= eps) //Проверка промежуточного корня
+			{
+				rule = false; //Цикл завершается
+			}
+			else {
+				x = xi; //Присваиваем новое значение, чтобы найти следующее приблизительное значение корня
+				cout << "Промежуточное значение " << n << " = " << x << endl; //Вывож промежуточного значения
+				n++;
+			}
+		}
+		answer = x;
+		cout << "Ответ равен: " << answer << endl;
+	}
+	else {
+		cout << "x не входит в промежуток [0; 0.85].";
+	}
+	return 0;
+}
